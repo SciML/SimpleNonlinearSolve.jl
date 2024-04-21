bad6a774d30df45d44ec3d040323093e4aacf6ee
Fabian Gittins <fabiangittins@gmail.com>
2024-04-21 09:04:04 +0100

    Fixed tests using Floats

--- test/core/muller_tests.jl
+++ test/core/muller_tests.jl
@@ -2,14 +2,14 @@
     @testset "Quadratic function" begin
         f(u, p) = u^2 - p
 
-        u0 = (10, 20, 30)
-        p = 612
+        u0 = (10.0, 20.0, 30.0)
+        p = 612.0
         prob = NonlinearProblem{false}(f, u0, p)
         sol = solve(prob, SimpleMuller())
 
         @test sol.u ≈ √612
 
-        u0 = (-10, -20, -30)
+        u0 = (-10.0, -20.0, -30.0)
         prob = NonlinearProblem{false}(f, u0, p)
         sol = solve(prob, SimpleMuller())
 
@@ -19,13 +19,13 @@
     @testset "Sine function" begin
         f(u, p) = sin(u)
 
-        u0 = (1, 2, 3)
+        u0 = (1.0, 2.0, 3.0)
         prob = NonlinearProblem{false}(f, u0)
         sol = solve(prob, SimpleMuller())
 
         @test sol.u ≈ π
 
-        u0 = (2, 4, 6)
+        u0 = (2.0, 4.0, 6.0)
         prob = NonlinearProblem{false}(f, u0)
         sol = solve(prob, SimpleMuller())
 
@@ -35,19 +35,19 @@
     @testset "Exponential-sine function" begin
         f(u, p) = exp(-u)*sin(u)
 
-        u0 = (-2, -3, -4)
+        u0 = (-2.0, -3.0, -4.0)
         prob = NonlinearProblem{false}(f, u0)
         sol = solve(prob, SimpleMuller())
 
         @test sol.u ≈ -π
 
-        u0 = (-1, 0, 1/2)
+        u0 = (-1.0, 0.0, 1/2)
         prob = NonlinearProblem{false}(f, u0)
         sol = solve(prob, SimpleMuller())
 
         @test sol.u ≈ 0
 
-        u0 = (-1, 0, 1)
+        u0 = (-1.0, 0.0, 1.0)
         prob = NonlinearProblem{false}(f, u0)
         sol = solve(prob, SimpleMuller())
 
